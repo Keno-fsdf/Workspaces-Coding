85b86dd7d6c136a2568c565bd9fb08d909fe089f
Keno-fsdf <k.schuerger2004@gmail.com>
2023-10-18 11:17:25 +0200

    Datenbanken

--- Datenbanken/mysql.sql
+++ Datenbanken/mysql.sql
@@ -1,4 +1,4 @@
-CREATE TABLE emp (empid INTEGER NOT NULL PRIMARY KEY, empname TEXT NOT NULL, email NOT NULL);
-INSERT INTO emp (empid, empname, email)
-VALUES (1, "Sam", "test1@test.com");
-SELECT * From emp;
+-- Create Table emp(empid INTEGER NOT NULL PRIMARY KEY, empname, email);
+-- INSERT INTO emp (empid, empname, email)
+-- VALUES (2, "Tom", "test2@test.com");
+SELECT * From emp; 
