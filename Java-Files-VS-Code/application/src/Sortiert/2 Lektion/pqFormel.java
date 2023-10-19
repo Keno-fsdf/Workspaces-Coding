21d80dccf6ac53fd14b6c79629ad589302f5613e
Keno-fsdf <k.schuerger2004@gmail.com>
2023-10-19 10:08:29 +0200

    Update pqFormel

--- Java-Files-VS-Code/application/src/Sortiert/2 Lektion/pqFormel.java
+++ Java-Files-VS-Code/application/src/Sortiert/2 Lektion/pqFormel.java
@@ -2,8 +2,8 @@ public class pqFormel {
     public static void main(String[] args)  {
         int p = 10; 
         int q = 20;
-        double berechnungErsteNullstelle = (-p/2) + Math.sqrt(Math.pow(p/2,2) -q); //berechnet die erste nullstelle
-        double berechnungZweiteNullstelle = (-p/2) - Math.sqrt(Math.pow(p/2,2) -q); //berechnet die erste nullstelle
+        double berechnungErsteNullstelle = (-p/2) + Math.sqrt(((p/2)*(p/2)) -q); //berechnet die erste nullstelle
+        double berechnungZweiteNullstelle = (-p/2) - Math.sqrt(((p/2)*(p/2)) -q); //berechnet die erste nullstelle
         System.out.println("Die Nullstellen sind " + berechnungErsteNullstelle + " und " + berechnungZweiteNullstelle);  //Ich gebe das Ergbnis im Terminal aus. 
     }    
     
