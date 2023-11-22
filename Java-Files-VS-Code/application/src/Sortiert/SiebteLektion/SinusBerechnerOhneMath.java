0d7ae94ed24dc158a17ab68078b77bef48db11fa
Keno-fsdf <k.schuerger2004@gmail.com>
2023-11-22 19:21:21 +0100

    update

--- Java-Files-VS-Code/application/src/Sortiert/SiebteLektion/SinusBerechnerOhneMath.java
+++ Java-Files-VS-Code/application/src/Sortiert/SiebteLektion/SinusBerechnerOhneMath.java
@@ -26,14 +26,18 @@ public class SinusBerechnerOhneMath {
 
     // Eigene Implementierung der Sinusfunktion
     private static double berechneSinus(double winkelRadiant) {
-        double result = 0;
-        double term = winkelRadiant;
+        double result = 0;  //Hier wird eine Variable result deklariert und auf 0 gesetzt. Diese Variable wird verwendet, um das Ergebnis der Sinus-Berechnung zu speichern.
+        double term = winkelRadiant;  // Eine weitere Variable term wird deklariert und mit dem Eingabewert winkelRadiant initialisiert. 
+        //Dieser Term wird in jedem Schleifendurchlauf aktualisiert und zur result-Variable hinzugefügt.
 
-        for (int i = 1; i <= 10; i += 2) {
-            result += term;
-            term *= -1 * winkelRadiant * winkelRadiant / ((i + 1) * (i + 2));
+
+        for (int i = 1; i <= 10000; i += 2) {    //diese 10000 gibt einfach an wie oft die for-Schleife läuft -->Durch jede weitere Berechnung wird das Ergebnis genauer
+                                                //man könnte also auch diesen Wert erhöhen um das Ergebnis minimal genauer zu haben -->time complexitiy: O(n)
+            result += term;     // Bei der ersten Iteration wird winkelRadiant hinzugefügt, danach immer das Ergebnis von term in der Zeile darunter 
+            //dadurch wird auch suzessiv das Ergebnis genauer.                 
+            term *= -1 * winkelRadiant * winkelRadiant / ((i + 1) * (i + 2));  //Der Term wird aktualisiert, um den nächsten Beitrag zur Taylor-Reihe zu repräsentieren
+            //. Dies geschieht durch Multiplikation mit dem Verhältnis von zwei aufeinanderfolgenden Zahlen
         }
 
-        return result;
-    }
-}
+        return result;    //Hier wird das ERgebnis zurückggeben
+}}
--- Java-Files-VS-Code/application/src/Sortiert/SiebteLektion/arithemtialMittel.java
+++ Java-Files-VS-Code/application/src/Sortiert/SiebteLektion/arithemtialMittel.java
@@ -9,59 +9,100 @@ public class arithemtialMittel {
         System.out.println("Anzahl der einzulesenden Werte:");
         int anzahlDerWerte = scanner.nextInt();
 
+
+        // Überprüft ob mehr als 0 Werte vom Benutzer eingeben wurden
         if (anzahlDerWerte <= 0) {
             System.out.println("Eingebener Wert ist zu klein. Er muss größer als 0 sein");
             scanner.close();
             return;
         }
 
-        double zahlen[] = new double[anzahlDerWerte];
+
+        
+
+
+
+
+
+
+
+
+
+
+
+        double zahlen[] = new double[anzahlDerWerte];  //array = {3,6; 5,5
+
         int zwischenspeicher = 0;
 
-        while (zwischenspeicher < anzahlDerWerte) {
+        
+            
+
+        while (zwischenspeicher < anzahlDerWerte) {  // zwischenspeicher = 1   < anzahlDerWerte = 3
             System.out.println("Geben Sie den " + (zwischenspeicher + 1) + ". Wert an: ");
             zahlen[zwischenspeicher] = scanner.nextDouble();
             zwischenspeicher++;
         }
 
-        double minimum = zahlen[0];
-        double maximum = zahlen[0];
-        double summe = zahlen[0]; // Ist für Mittelwert
-        double quadDistanz = 0;
-        for (int i = 1; i < zahlen.length; i++) {
-            summe = summe + zahlen[i];
 
-            if (zahlen[i] < minimum) {
-                minimum = zahlen[i];
+
+
+
+        double minimum = zahlen[0];  // minimum = 3,6
+
+        double maximum = zahlen[0]; // maximum = 3,6 
+        double summe = zahlen[0]; // Ist für Mittelwert
+     
+
+
+
+        for (int i = 1; i < zahlen.length; i= i+1) {
+           
+            summe = summe + zahlen[i];  //hier berechene ich die summe aller werte in der array
+            // 9,1 = 3,6 + 5,5 
+            //summe = 9,1 
+
+            //9,1 = 9,1 + 6,6 
+            //summe = 15,7
+
+
+            if (zahlen[i] < minimum) {  // 2 < 3,6
+                minimum = zahlen[i]; //minimum = 2
             }
 
             if (zahlen[i] > maximum) {
                 maximum = zahlen[i];
             }
-            // double distanz = zahlen[i] -mittelwert
-            
+           
            
 
         }
-        
-        double mittelwert = summe / zahlen.length;
+
+        double quadDistanz = 0;
+        double mittelwert = summe / zahlen.length;  //  2+ 3, 6 = 9  /3  --> 3
 
         for (int i = 0; i<zahlen.length; i++) {
-         double distanz = zahlen[i] -mittelwert;
+
+         double distanz = zahlen[i] -mittelwert;   // 3,6 - 3,5333 
          quadDistanz = quadDistanz+ (distanz*distanz); }
-        double standardabweichung = Math.sqrt(quadDistanz / zahlen.length ); 
+
+
+
+    double standardabweichung = Math.sqrt(quadDistanz / zahlen.length); 
+
+   
         
 
 
 
-        System.out.println(Arrays.toString(zahlen));
+        // System.out.println(Arrays.toString(zahlen));
         System.out.println("Standardsabweichung: " + standardabweichung);
         System.out.println("Mittelwert: " + mittelwert);
         System.out.println("Maximum: " + maximum);
         System.out.println("Minimum: " + minimum);
-        System.out.println("Summe: " + summe);
+        // System.out.println("Summe: " + summe);
 
         // Scanner schließen
         scanner.close();
+        
     }
 }
